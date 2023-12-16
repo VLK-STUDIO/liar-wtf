33c194a8ccb929220fdc84d59c107a5148382fad
plushdohn <lucafarci@protonmail.com>
2023-12-16 23:50:27 +0100

    fix: vertical overflow in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <main
-    class="h-[100dvh] overflow-y-auto overflow-x-hidden flex flex-col justify-center items-center gap-12 max-w-sm p-8 mx-auto"
+    class="min-h-[100dvh] overflow-y-auto overflow-x-hidden flex flex-col justify-center items-center gap-12 max-w-sm p-8 mx-auto"
   >
     <slot />
   </main>
