5b2b0af6d7cdc865f8cb717a9f782b474477a595
plushdohn <lucafarci@protonmail.com>
2023-12-20 18:42:28 +0100

    chore: remove client-side console logs

--- stores/game.ts
+++ stores/game.ts
@@ -48,8 +48,6 @@ export const useGameStore = defineStore("game", {
           const event = JSON.parse(message.data) as GameEvent;
 
           if (event.type === "STATE_UPDATE") {
-            console.log("State update", event);
-
             this.state = {
               socket,
               userId,
@@ -90,8 +88,6 @@ export const useGameStore = defineStore("game", {
           event.type === "PLAYER_RECONNECTED" ||
           event.type === "PLAYER_DISCONNECTED"
         ) {
-          console.log("Player connection event", event);
-
           callback(event);
         }
       }
@@ -125,8 +121,6 @@ export const useGameStore = defineStore("game", {
         type: "REQUEST_NEW_TOPIC",
       });
 
-      console.log("New topic", response.newTopic);
-
       this.state.topic = response.newTopic;
     },
     getUserId() {
@@ -144,8 +138,6 @@ export const useGameStore = defineStore("game", {
     },
     async socketFetch(request: PartyClientRequest) {
       if (this.state.phase === "INIT") return;
-
-      console.log({ host: PARTYKIT_HOST, roomId: this.state.roomId });
 
       const json: any = await $fetch(
         `${PARTYKIT_HOST}/parties/main/${this.state.roomId}`,
